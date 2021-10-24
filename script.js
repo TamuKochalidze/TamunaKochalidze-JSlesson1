25cf658fbbfc4e1bdfed1c4430a8ad7ea53acfaf
Tamuna <85788033+tamuna-kochalidze@users.noreply.github.com>
2021-10-24 19:36:21 +0400

    lesson 1 homework 2

---
+++ script.js
@@ -0,0 +1,40 @@
+//davaleba-1 
+//ამოიღეთ user ცვლადის სტუდენტის სტატუსი
+
+// let user = {
+//     firstName: 'giorgi',
+//     lastName: 'smith',
+//     age: 25,
+//     studentstatus: 'active'
+// };
+// console.log(user.studentstatus);
+// console.log(user['studentstatus']);
+//----------------------------------
+
+//davaleba-2
+//შექმენით მასივი, და დაბეჭდეთ სათითაოდ თითოეული ელემენტი; (for, while ციკლების საშუალებით)
+
+// let friends = ['giorgi', 'mari', 'daviti',]
+// for (let x = 0; x < friends.length; x++) {
+//   console.log(friends[x]);  
+// }
+
+// for (let item of friends) {
+//     console.log(item);
+// }
+
+// let item = 0;
+// whine (item < friends.length) {
+//     console.log(friends[item]);
+//     item++
+// }
+//--------------------------------------
+
+//davaleba-3
+
+//davaleba-4
+//davaleba-5
+//davaleba-6
+//davaleba-7
+//davaleba-8
+//davaleba-9
