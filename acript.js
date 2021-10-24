ad07c36ead7504abaaf47f38bea1467fb0939639
Tamuna <85788033+tamuna-kochalidze@users.noreply.github.com>
2021-10-24 19:17:57 +0400

    homework number 1

---
+++ acript.js
@@ -0,0 +1,19 @@
+//davaleba-1
+
+let user = {
+    firstName: 'giorgi',
+    lastName: 'smith',
+    age: 25,
+    studentstatus: 'active'
+};
+console.log(user.studentstatus);
+console.log(user['studentstatus']);
+//----------------------------------
+//davaleba-2
+//davaleba-3
+//davaleba-4
+//davaleba-5
+//davaleba-6
+//davaleba-7
+//davaleba-8
+//davaleba-9
